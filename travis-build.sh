57981819fdf39c75df314437d1ae9e19acd270e2
graemerocher <graeme.rocher@gmail.com>
2016-06-06 10:29:37 +0200

    Add Gradle plugin publishing

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-./gradlew clean test assemble
+./gradlew clean assemble
 
 EXIT_STATUS=0
 echo "Publishing archives for branch $TRAVIS_BRANCH"
@@ -10,6 +10,9 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
 
   if [[ -n $TRAVIS_TAG ]]; then
       ./gradlew bintrayUpload || EXIT_STATUS=$?
+      if [[ $EXIT_STATUS == 0 ]]; then
+        ./gradlew publishPlugins -Dgradle.publish.key="$GRADLE_PUBLISH_KEY" -Dgradle.publish.secret="$GRADLE_PUBLISH_SECRET" || EXIT_STATUS=$?
+      fi
   fi
 
 fi
